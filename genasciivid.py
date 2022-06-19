b118e0b0ffc114d01b730c0e0bcabcabdde9036d
shrayashp <76247110+shrayashp@users.noreply.github.com>
2022-06-19 13:56:21 +0530

    Update genasciivid.py

--- genasciivid.py
+++ genasciivid.py
@@ -1,7 +1,9 @@
-import cv2, argparse, os
+import cv2
+import argparse
+import os
+import shutil
 import numpy as np
 from PIL import Image, ImageDraw, ImageOps, ImageFont
-import shutil
 
 # Characters used for Mapping to Pixels
 Character = {
@@ -9,16 +11,15 @@ Character = {
     "complex": "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'. "
 }
 
-
+# Function to define font and which character list is used
 def get_data(mode):
     font = ImageFont.truetype("fonts\DejaVuSansMono.ttf", size = 20)
     char_list = Character[mode]
     return char_list, font,
 
-
-# Making Background Black or White
-
+# Function to define font and which character list is used
 def genAscii(fname, scale, num_cols, outfname):
+    # Making Background Black or White
     bg = "black"
     #bg = "white"
     if bg=="white":
@@ -26,49 +27,32 @@ def genAscii(fname, scale, num_cols, outfname):
     elif bg=="black":
         bg_code=(0, 0, 0)
 
-
-    # Getting the character List, Font and Scaling characters for square Pixels
+    # Getting the character List and Font
     char_list, font = get_data("complex")
     num_chars = len(char_list)
-    #num_cols = 275
+
     # Reading Input Image
     im= cv2.imread(fname)
 
-    # Converting Color Image to Grayscale
-    #im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
     # Extracting height and width from Image
-
     height, width, _ = im.shape
-    # Defining height and width of each cell==pixel
 
+    # Defining height and width of each cell==pixel
     cell_w = width/num_cols
     cell_h = int(scale*cell_w)
     num_rows = int(height/cell_h)
-    # Calculating Height and Width of the output Image
 
+    # Calculating Height and Width of the output Image
     char_w, char_h = font.getsize("A")
     out_width = char_w*num_cols
     out_height = int(char_h*scale*num_rows)
 
 
     # Making a new Image using PIL
-
     out_im = Image.new("RGB", (out_width, out_height), bg_code)
     draw = ImageDraw.Draw(out_im)
 
-    # Mapping the Characters
-    # for i in range(num_rows):
-    #     min_h = min(int((i+1)*cell_h), height)
-    #     row_pix = int(i*cell_h)
-    #     # lst = [i for i in range(5)] => We can make strings/lists/tuples in this way => lst = [0, 1, 2, 3, 4]
-    #     # lst[first:last] gives us a sublist from the first index to the last index excluding the last index => lst[1:4]==[1, 2, 3]
-    #     line = "".join([char_list[min(int(np.mean(im[row_pix:min_h, int(j * cell_w):min(int((j + 1) * cell_w), width)]) / 255 * num_chars), num_chars - 1)]for j in range(num_cols)]) + "\n"
-    #
-    #     # Draw string at a given position (x,y)
-    #     draw.text((0, i*char_h), line, fill = 255-bg_code, font = font)
-
-    #mapping characters for RGB
-
+    # Mapping characters for RGB
     for i in range(num_rows):
         for j in range(num_cols):
             snapshot = im[int(i*cell_h):min(int((i+1)*cell_h), height), int(j*cell_w):min(int((j+1)*cell_w), width),:]
@@ -88,42 +72,52 @@ def genAscii(fname, scale, num_cols, outfname):
     out_im = out_im.crop(cropped_im)
     out_im.save("./out1.jpg")
 
+
+# Function to create a folder containing ascii art of all frames of a video
 def convert_to_frames(fname, num_cols):
+    # Reading input video
     vid = cv2.VideoCapture(fname)
     try:
-
-        # creating a folder named data
+        # Creating a folder named data
         if not os.path.exists('data'):
             os.makedirs('data')
-
-    # if not created then raise error
+    # If not created then raise error
     except OSError:
         print('Error: Creating directory of data')
 
-    # frame
+    # Defining frame number
     currentframe = 0
 
     while (True):
 
-        # reading from frame
+        # Reading from frame
         success, frame = vid.read()
 
         if success:
-            # continue creating images until video remains
+
+            # Defining names for frame and ascii art of frame
             name = './data/frame' + str(currentframe) + '.jpg'
             name2 = './data/outframe' + str(currentframe) + '.jpg'
             print('Creating...' + name)
             #print(frame)
-            # writing the extracted images
+
+            # Writing the extracted frames to folder
             cv2.imwrite(name, frame)
+
+            # Creating ascii art of frames
             genAscii(name, 2, num_cols, name2)
+
+            # Writing ascii art of frame to folder
             im1= cv2.imread("./out1.jpg")
             cv2.imwrite(name2, im1)
+
+            # Deleting the original frame after ascii art has been added to the folder
             try:
                 os.remove(name)
             except:
                 pass
-            # increasing counter so that it will
+
+            # Increasing counter so that it will
             # show how many frames are created
             currentframe += 1
         else:
@@ -133,30 +127,42 @@ def convert_to_frames(fname, num_cols):
     vid.release()
     cv2.destroyAllWindows()
 
+# Function to compile given folder of frames to a video
 def convert_frames_to_video(output_file_name, fps):
 
+    # Defining path of frames folder
     path = os.getcwd()
     data_dir = 'data'
     input_frame_path = os.path.join(path, data_dir)
     input_list = os.listdir(input_frame_path)
+    
+    #Defining size of frame
     frame = cv2.imread(os.path.join(input_frame_path, 'outframe0.jpg'))
     height, width, channels = frame.shape
-    # Define the codec.FourCC is a 4-byte code used to specify the video codec
-    fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # Be sure to use lower case
+    
+    # Defining the codec.FourCC is a 4-byte code used to specify the video codec
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
     size = (width, height)
-    # Define the output video writer object
+    
+    # Defining the output video writer object
     out = cv2.VideoWriter(output_file_name, fourcc, fps, size)
     num_frames = len(input_list)
 
+    # Adding frame to video
     for i in range(num_frames):
         base_name = 'outframe'
         img_name = base_name + '{:1d}'.format(i) + '.jpg'
         img_path = os.path.join(input_frame_path, img_name)
         img = cv2.imread(img_path)
-        out.write(img)  # Write out frame to video
-    # Release everything if job is finished
+        
+        # Writing out frame to video
+        out.write(img)
+        
+    # Release all space and windows once done
     out.release()
-    print("The output video is {} is saved".format(output_file_name))
+    print("The output ascii art video is {} is saved".format(output_file_name))
+    
+    # Destroying the folder once output video is saved
     try:
         shutil.rmtree(data_dir)
     except:
@@ -165,7 +171,8 @@ def convert_frames_to_video(output_file_name, fps):
 def main():
     descStr = "This program converts a video into ASCII art."
     parser = argparse.ArgumentParser(description=descStr)
-    # add expected arguments
+    # add arguments for input file, output file
+    # fps and number of columns in final image
     parser.add_argument('--file', dest='vidFile', required=True)
     parser.add_argument('--out', dest='outFile', required=False)
     parser.add_argument('--fps', dest= 'fps', required=False)
@@ -184,8 +191,6 @@ def main():
         fps = int(args.fps)
     convert_to_frames(vidFile, num_cols)
     convert_frames_to_video(outFile, fps)
-
-
 
 if __name__ == '__main__':
     main()
